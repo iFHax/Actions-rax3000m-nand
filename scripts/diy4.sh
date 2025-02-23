2d82f041100233f27fcfc63dd627fa997913b198
liedieyou <76842456+liedieyou@users.noreply.github.com>
2025-02-24 03:11:08 +0800

    Update diy4.sh

--- scripts/diy4.sh
+++ scripts/diy4.sh
@@ -1,2 +1,3 @@
 #把pcie_mhi从1.3.6更新到1.3.8
-sed -i 's/+kmod-pcie_mhi/+kmod-mhi-pcie/g' feeds/extraipk/luci-app-modem/luci-app-modem/Makefile
+#sed -i 's/+kmod-pcie_mhi/+kmod-mhi-pcie/g' feeds/extraipk/luci-app-modem/luci-app-modem/Makefile
+sed -i 's/+kmod-pcie_mhi//g' feeds/extraipk/luci-app-modem/luci-app-modem/Makefile
